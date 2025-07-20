78d5ff5ba337fb20df0be319c70af780c892116c
Edwin Chan <eneon54321@gmail.com>
2025-07-20 16:45:23 +0800

    complete with gradio demo

--- gradio-dashboard.py
+++ gradio-dashboard.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from langchain_community.document_loaders import TextLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_chroma import Chroma
 
@@ -37,10 +37,52 @@ def retrieve_semantic_recommendations(query: str, category: str = None, tone: st
         "Happy": "joy",
         "Surprising": "surprise",
         "Angry": "anger",
+        "Suspenseful": "fear",
         "Sad": "sadness",
+        "All": 'neutral'
     }
-    if tone == 'Happy':
-        sort_by = tone_sort_map.get(tone, 'neutral')
-        books_recs.sort_values(by=sort_by, ascending=False, inplace=True)
+    sort_by = tone_sort_map.get(tone, 'neutral')
+    books_recs.sort_values(by=sort_by, ascending=False, inplace=True)
 
     return books_recs
+
+def get_recommendations(query: str, category: str = None, tone: str = None):
+    recommendations = retrieve_semantic_recommendations(query, category, tone)
+    results = []
+
+    for _, row in recommendations.iterrows():
+        # Truncate description if more than 30 words
+        description = row["description"]
+        truncated_desc_split = description.split()
+        truncate_limit = 30
+        truncated_description = " ".join(truncated_desc_split[:truncate_limit]) + ("..." if len(truncated_desc_split) > truncate_limit else "")
+
+        authors_split = row["authors"].split(';')
+        authors_str = ", ".join(authors_split[:-1]) + " and " + authors_split[-1] if len(authors_split) > 1 else authors_split[0]
+
+        caption = f"{row['title']} by {authors_str}: {truncated_description}"
+        results.append((row['large_thumbnail'], caption))
+
+    return results
+
+categories = ["All"] + sorted(books['simple_categories'].unique())
+tones = ["All", "Happy", "Surprising", "Angry", "Surprising", "Sad"]
+
+# Gradio configs
+with gr.Blocks(theme=gr.themes.Glass()) as dashboard:
+    gr.Markdown('# Semantic book recommender')
+
+    with gr.Row():
+        user_query = gr.Textbox(label="Please enter a book's description.", placeholder="ex: a story about forgiveness")
+        category_dropdown = gr.Dropdown(choices=categories, label="Select a category:", value="All")
+        tone_dropdown = gr.Dropdown(choices=tones, label="Select an emotional tone:", value="All")
+        submit_button = gr.Button("Find recommendations")
+
+    gr.Markdown('## Recommendations')
+    outputs = gr.Gallery(label="Recommended books", columns=8, rows=2)
+
+    submit_button.click(fn=get_recommendations, inputs=[user_query, category_dropdown, tone_dropdown], outputs=outputs)
+
+if __name__ == '__main__':
+    dashboard.launch(share=True)
+
